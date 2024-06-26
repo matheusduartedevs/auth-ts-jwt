43fad0a530c63dbf8372ada01b0288e942740064
unknown <matheus.duartesilva42@gmail.com>
2024-06-26 20:21:48 -0300

    feat: implementação da rota get de buscar todos os usuários

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,6 +3,11 @@ import { prisma } from "../utils/prisma";
 import { hash } from "bcryptjs";
 
 export class UserController {
+    async index (req: Request, res: Response) {
+        const users = await prisma.user.findMany()
+        return res.json({ users })
+    }
+
     async store (req: Request, res: Response) {
         const { name, email, password } = req.body
         const hash_password = await hash(password, 8)
--- src/routes.ts
+++ src/routes.ts
@@ -5,4 +5,5 @@ export const router = Router()
 
 const usecontroller = new UserController()
 
+router.get('/users', usecontroller.index)
 router.post('/create', usecontroller.store)
