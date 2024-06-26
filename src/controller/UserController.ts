927c0e2105a1f56ce57670004510f79464a8eef3
unknown <matheus.duartesilva42@gmail.com>
2024-06-26 20:18:23 -0300

    feat: implementação da encriptação da senha do usuário

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from "express";
 import { prisma } from "../utils/prisma";
+import { hash } from "bcryptjs";
 
 export class UserController {
     async store (req: Request, res: Response) {
         const { name, email, password } = req.body
-
+        const hash_password = await hash(password, 8)
+        
         const user = await prisma.user.create({
             data: {
                 name,
                 email,
-                password
+                password: hash_password
             }
         })
 
