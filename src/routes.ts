5b61929594d76865dceb876e2a2482a8d5094746
unknown <matheus.duartesilva42@gmail.com>
2024-06-27 09:53:21 -0300

    feat: implementando middleware de autenticação e adicionando na rota

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { UserController } from "./controller/UserController";
 import { AuthController } from "./controller/AuthController";
+import { AuthMiddleware } from "./middlewares/auth";
 
 export const router = Router()
 
 const usecontroller = new UserController()
 const authcontroller = new AuthController()
 
-router.get('/users', usecontroller.index)
+router.get('/users', AuthMiddleware, usecontroller.index)
 router.post('/create', usecontroller.store)
 router.post('/auth', authcontroller.authenticate)
