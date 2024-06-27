f11a2a0f66ebae71a27d5120fc92590e0010bf91
unknown <matheus.duartesilva42@gmail.com>
2024-06-27 09:22:41 -0300

    feat: implementação do controller de autenticação e rota auth

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { UserController } from "./controller/UserController";
+import { AuthController } from "./controller/AuthController";
 
 export const router = Router()
 
 const usecontroller = new UserController()
+const authcontroller = new AuthController()
 
 router.get('/users', usecontroller.index)
 router.post('/create', usecontroller.store)
+router.post('/auth', authcontroller.authenticate)
